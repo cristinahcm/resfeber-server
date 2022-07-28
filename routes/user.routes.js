1c6e1f661bb74eb11c45185f4e055b19ff24673c
Jessica Fernandez <jferasef@gmail.com>
2022-07-28 18:42:39 +0200

    user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,33 +4,46 @@ const router = express.Router();
 const Travel = require("../models/Travel.model");
 const User = require("../models/User.model");
 
-// route to find userbyid
-// router.get("/",isAuthenticated, async (req, res) => {
-//   try {
-//     res.status(200).json(req.payload);
-//     console.log(req.payload);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-router.get("/", isAuthenticated, async (req, res, next) => {
+router.get("/", isAuthenticated, async (req, res) => {
   try {
-    const users = await User.find();
-    // find with filters with the info to get
-    // const userFiltered = users.filter(
-    //   (user) =>
-    //     user.username  ||
-    //     user.email ||
-    //     user
-    // );
-    return res.status(200).json(users);
+    res.status(200).json(req.payload);
+    console.log(req.payload);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:id", isAuthenticated, async (req, res, next) => {
+  try {
+    const users = await User.findById(req.params.id);
+    const userFiltered = users.filter(
+      (user) =>
+        user.username ||
+        user.email ||
+        user.picture ||
+        user.interests ||
+        user.gender ||
+        user.comments ||
+        user.age
+    );
+    return res.status(200).json(userFiltered);
   } catch (error) {
     next(error);
   }
 });
 
 // router post to use cloudinary
+router.put("/upload", isAuthenticated, async (req, res, next) => {
+  try {
+    const { picture } = req.body;
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      picture,
+    });
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.put("/edit/:id", isAuthenticated, async (req, res, next) => {
   try {
